52a5fad05ecec5d327b90505f21cdddac32e9371
kitare17 <hoctapak@gmail.com>
2024-01-22 23:47:57 +0700

    xong frontend v1 -Home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import Footer from './component/header-footer/Footer';
+import Header from "./component/header-footer/Header";
+import Home from "./component/include/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header></Header>
+            <Home></Home>
+            <Footer></Footer>
+
+            <div>
+                <p className=''></p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
