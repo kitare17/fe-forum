6dd0d25b975a6fcee525430f312fd1620bd44c12
quang <quanggaming2002@gmail.com>
2024-03-08 10:32:02 +0700

    register + update

--- src/component/include/Register.js
+++ src/component/include/Register.js
@@ -1,6 +1,37 @@
-import {Link} from "react-router-dom";
+import {useContext, useState} from "react";
+import {Link, useNavigate} from "react-router-dom";
+import axios from "axios";
 
 function Register() {
+
+    const navigate=useNavigate();
+    const [formRegister, setFormRegister] = useState({
+        username: "",
+        password: "",
+        phone:"",
+        email:"",
+        fullname: ""
+    })
+
+    const [message, setMessage] = useState('');
+
+    const handleChange = (e) => {
+        console.log(e.target.name)
+        console.log(e.target.value)
+        setFormRegister({...formRegister, [e.target.name]: e.target.value});
+    }
+
+    const handleSummit = async () => {
+        console.log(formRegister);
+        const dataRes = await axios.post("http://localhost:3001/users/register", formRegister);
+        if (dataRes.data !== "successfully") {
+            setMessage("fail")
+        } else {
+        setMessage(dataRes.data)
+            
+        }
+        
+    }
     return (
         <div>
             <div style={{backgroundColor: "#ff9326"}} className="container-fluid py-5">
@@ -15,35 +46,35 @@ function Register() {
                                 <div className="row g-3">
                                     <div className="col-sm-6">
                                         <div className="form-floating">
-                                            <input type="text" className="form-control border-0" id="gname"
+                                            <input onChange={handleChange} type="text" className="form-control border-0" id="gname" name = "fullname"
                                                    placeholder="Gurdian Name"/>
                                             <label htmlFor="gname">Họ và tên</label>
                                         </div>
                                     </div>
                                     <div className="col-sm-6">
                                         <div className="form-floating">
-                                            <input type="email" className="form-control border-0" id="gmail"
+                                            <input onChange={handleChange} type="email" className="form-control border-0" id="gmail" name = "email"
                                                    placeholder="Gurdian Email"/>
                                             <label htmlFor="gmail"> Email</label>
                                         </div>
                                     </div>
                                     <div className="col-sm-6">
                                         <div className="form-floating">
-                                            <input type="text" className="form-control border-0" id="cname"
+                                            <input onChange={handleChange} type="text" className="form-control border-0" id="cname" name = "phone"
                                                    placeholder="Child Name"/>
                                             <label htmlFor="cname">Số điện thoại</label>
                                         </div>
                                     </div>
                                     <div className="col-sm-6">
                                         <div className="form-floating">
-                                            <input type="text" className="form-control border-0" id="cage"
+                                            <input onChange={handleChange} type="text" className="form-control border-0" id="cage" name = "username"
                                                    placeholder="Child Age"/>
                                             <label htmlFor="cage">Tên đăng nhập</label>
                                         </div>
                                     </div>
                                     <div className="col-sm-6">
                                         <div className="form-floating">
-                                            <input type="password" className="form-control border-0" id="cage"
+                                            <input onChange={handleChange} type="password" className="form-control border-0" id="cage" name = "password"
                                                    placeholder="Child Age"/>
                                             <label htmlFor="cage">Mật khẩu</label>
                                         </div>
@@ -55,12 +86,15 @@ function Register() {
                                             <label htmlFor="cage">Nhập lại mật khẩu</label>
                                         </div>
                                     </div>
-
+                                    <div>
+                                        {/* Hiển thị thông báo */}
+                                        <p>{message}</p>
+                                    </div>
                                     <div className="col-12  container">
                                         <div className="row">
                                             <Link to='/login' className="col-md-5 m-md-0 m-3" >Đã có tài khoản?</Link>
 
-                                            <button className="btn btn-primary py-3 px-4 col-md-2" type="submit">Đăng kí</button>
+                                            <button onClick={handleSummit} className="btn btn-primary py-3 px-4 col-md-2" type="submit">Đăng kí</button>
                                         </div>
 
                                     </div>
--- src/component/include/UpdateProfile.js
+++ src/component/include/UpdateProfile.js
@@ -1,6 +1,36 @@
-
+import {useContext, useState} from "react";
+import {Link, useNavigate} from "react-router-dom";
+import axios from "axios";
 
 function UpdateProfile(){
+    const navigate=useNavigate();
+    const [formUpdate, setFormUpdate] = useState({
+        username: "",
+        password: "",
+        phone:"",
+        email:"",
+        fullname: ""
+    })
+
+    const [message, setMessage] = useState('');
+
+    const handleChange = (e) => {
+        console.log(e.target.name)
+        console.log(e.target.value)
+        setFormUpdate({...formUpdate, [e.target.name]: e.target.value});
+    }
+
+    const handleSummit = async () => {
+        console.log(formUpdate);
+        const dataRes = await axios.post("http://localhost:3001/users/update-profile", formUpdate);
+        if (dataRes.data !== "successfully") { //fix cho nay la xong ? sua ca BE
+            setMessage("fail")
+        } else {
+        setMessage(dataRes.data)
+            
+        }
+    }
+
     return(
         <div>
 
@@ -8,31 +38,34 @@ function UpdateProfile(){
                 <h2 className="text-center">Cập nhật thông tin cá nhân</h2>
                 <form action="">
 
-
                 <div className="form-group">
                     <label htmlFor="username">Username:</label>
-                    <input type="text" className="form-control" id="username" value="username" disabled />
+                    <input  onChange={handleChange} type="text" className="form-control" id="username" value="username" disabled name="username" />
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password:</label>
-                    <input type="password" className="form-control" id="password" value="********" disabled />
+                    <input  onChange={handleChange} type="password" className="form-control" id="password" value="********" disabled name="password"/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="fullname">Fullname:</label>
-                    <input type="text" className="form-control" id="fullname" />
+                    <input  onChange={handleChange}  type="text" className="form-control" id="fullname" name="fullname" />
                 </div>
                 <div className="form-group">
                     <label htmlFor="email">Email:</label>
-                    <input type="email" className="form-control" id="email" />
+                    <input  onChange={handleChange} type="email" className="form-control" id="email" name = "email" />
                 </div>
                 <div className="form-group">
                     <label htmlFor="phone">Phone:</label>
-                    <input type="tel" className="form-control" id="phone" />
+                    <input  onChange={handleChange} type="tel" className="form-control" id="phone"name="phone" />
                 </div>
+                <div>
+                  {/* Hiển thị thông báo */}
+                    <p>{message}</p>
+                 </div>
                 <div className="text-center mb-4">
-                    <button className="btn btn-warning mr-2">Cập nhật</button>
+                    <button onClick={handleSummit}  className="btn btn-warning mr-2">Cập nhật</button>
                 </div>
-
+                
                 </form>
             </div>
 
