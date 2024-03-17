67bae45e6e54d66869371ceba8cfb38f046fe166
kitare17 <hoctapak@gmail.com>
2024-03-17 10:38:14 +0700

    post detail done

--- src/component/include/PostDetail.js
+++ src/component/include/PostDetail.js
@@ -3,6 +3,8 @@ import {useContext, useEffect, useState} from "react";
 import {UserContext} from "../context/UserContext";
 import axios from "axios"
 import {toast} from "react-toastify";
+import {Link} from "react-router-dom";
+
 
 function PostDetail() {
     //get postId
@@ -47,14 +49,15 @@ function PostDetail() {
                     console.log(response.data);
                     setPost({
                         ...post,
-                        ...response.data});
+                        ...response.data
+                    });
                 })
                 .catch(error => {
                     console.error('There was an error!', error);
                 });
         }
         handleGetPost();
-    }, [postId,submit]);
+    }, [postId, submit]);
     //
 
     //handle change textarea
@@ -84,15 +87,103 @@ function PostDetail() {
             });
     }
 
+
     function renderComment(comments) {
         if (comments)
             return (
                 <div>
-                    <h1>Comment</h1>
+                    <h4>Bình luận</h4>
                     {comments.map((comment, index) => (
                         <div key={index}>
-                            <p>{comment.detail}</p>
-                            <p>{comment.userComment.fullname}</p>
+                            <div className="container">
+                                <div className="col-12">
+                                    <div className="task"
+                                         draggable="true"
+                                         style={{
+                                             position: "relative",
+                                             color: "#2e2e2f",
+                                             cursor: "move",
+                                             backgroundColor: "#e7e3e3",
+                                             padding: "1rem",
+                                             borderRadius: "8px",
+                                             boxShadow: "rgba(99, 99, 99, 0.1) 0px 2px 8px 0px",
+                                             marginBottom: "1rem",
+                                             border: "3px dashed transparent",
+                                             maxƯidth: "350px",
+
+
+                                         }}
+
+                                    >
+                                        <div className="tags"
+                                             style={{
+                                                 width: "100%",
+                                                 display: "flex",
+                                                 alignItems: "center",
+                                                 justifyContent: "space-between"
+                                             }}
+                                        >
+                                            <div>
+                                                <img src="https://img.icons8.com/?size=50&id=9q3GMpxNIMjC&format=png"
+                                                     alt=""/>
+                                                <span className="tag"
+                                                      style={{
+                                                          borderRadius: "100px",
+                                                          padding: "4px 13px",
+                                                          fontSize: "12px",
+                                                          color: "#ffffff",
+                                                          backgroundColor: "#1389eb"
+                                                      }}
+                                                >{comment.userComment.fullname}</span>
+                                            </div>
+
+
+                                        </div>
+                                        <p style={{
+                                            fontSize: "15px",
+                                            margin: "1.2rem 0"
+
+                                        }}>{
+                                            comment.detail
+                                        }</p>
+                                        <div className="stats"
+                                             style={{
+                                                 position: "relative",
+                                                 width: "100%",
+                                                 color: "#9fa4aa",
+                                                 fontSize: "12px",
+                                                 display: "flex",
+                                                 alignItems: "center",
+                                                 justifyContent: "space-between"
+
+                                             }}
+                                        >
+                                            <div
+                                                style={{
+                                                    marginRight: "1rem",
+                                                    height: "20px",
+                                                    display: "flex",
+                                                    alignItems: 'center',
+                                                    cursor: 'pointer'
+
+                                                }}
+                                            >
+                                                <div>
+                                                    <img src="https://img.icons8.com/?size=20&id=106850&format=png"
+                                                         alt=""/>
+                                                    <span className="m-1">
+                                                        {new Date(comment.createdAt).getDate()}/{new Date(comment.createdAt).getMonth() + 1}/ {new Date(comment.createdAt).getFullYear()}
+                                                    </span>
+                                                </div>
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+                                </div>
+
+
+                            </div>
                         </div>
                     ))}
                 </div>
@@ -101,30 +192,184 @@ function PostDetail() {
     }
 
     return (
-        <div>Post detail {postId}
+        <div>
             <div>
-                Chi tiết
-                {
-                    post.title ?
-                        post.title + " " + post.title + " " + JSON.stringify(post.creator) + " " + post.createdAt : ""
-                }
-                Comment
+
+
+            </div>
+
+            <div className="container mt-3 mb-5">
+                <div className="col-12">
+                    <button className="btn">
+                        <img src="https://img.icons8.com/?size=30&id=86203&format=png" alt=""/>
+                        <Link to="/forum">Quay lại</Link>
+                    </button>
+
+                </div>
+
+                <div className="col-12">
+                    <div className="task"
+                         draggable="true"
+                         style={{
+                             position: "relative",
+                             color: "#2e2e2f",
+                             cursor: "move",
+                             backgroundColor: "#f6f1f1",
+                             padding: "1rem",
+                             borderRadius: "8px",
+                             boxShadow: "rgba(99, 99, 99, 0.1) 0px 2px 8px 0px",
+                             marginBottom: "1rem",
+                             border: "3px dashed transparent",
+                             maxƯidth: "350px",
+
+
+                         }}
+
+                    >
+                        <div className="tags"
+                             style={{
+                                 width: "100%",
+                                 display: "flex",
+                                 alignItems: "center",
+                                 justifyContent: "space-between"
+                             }}
+                        >
+                            <div>
+                                <img src="https://img.icons8.com/?size=50&id=9q3GMpxNIMjC&format=png" alt=""/>
+                                <span className="tag"
+                                      style={{
+                                          borderRadius: "100px",
+                                          padding: "4px 13px",
+                                          fontSize: "12px",
+                                          color: "#ffffff",
+                                          backgroundColor: "#1389eb"
+                                      }}
+                                >{post.creator.fullname}</span>
+                            </div>
+
+
+                        </div>
+                        <h3 style={{paddingLeft: "10px"}}>{post.title}</h3>
+                        <p style={{
+                            fontSize: "15px",
+                            margin: "1.2rem 0"
+
+                        }}>{
+                            post.detail
+                        }</p>
+                        <div className="stats"
+                             style={{
+                                 position: "relative",
+                                 width: "100%",
+                                 color: "#9fa4aa",
+                                 fontSize: "12px",
+                                 display: "flex",
+                                 alignItems: "center",
+                                 justifyContent: "space-between"
+
+                             }}
+                        >
+                            <div
+                                style={{
+                                    marginRight: "1rem",
+                                    height: "20px",
+                                    display: "flex",
+                                    alignItems: 'center',
+                                    cursor: 'pointer'
+
+                                }}
+                            >
+                                <div>
+                                    <img src="https://img.icons8.com/?size=20&id=106850&format=png"
+                                         alt=""/>
+                                    <span className="m-1">
+                                                        {new Date(post.createdAt).getDate()}/{new Date(post.createdAt).getMonth() + 1}/ {new Date(post.createdAt).getFullYear()}
+                                                    </span>
+                                </div>
+                                <div>
+                                    <img src="https://img.icons8.com/?size=20&id=jOjH1Mt48Fp1&format=png" alt=""/>
+
+                                    <span  className="m-1">
+                                         {post.comments.length}
+                                    </span>
+
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+
+
+            </div>
+
+
+            <div className="container">
+
                 {
                     post.title ?
                         renderComment(post.comments)
                         : <div></div>
                 }
             </div>
-            <div>{
-                userData ?
-                    <form action="" onSubmit={handleSummit}>
-                 <textarea name="detail" onChange={handleChange} value={commentPost.detail} id="" cols="30" rows="10">
+            {/*<div>{*/}
+            {/*    userData ?*/}
+            {/*        <form action="" onSubmit={handleSummit}>*/}
+            {/*     <textarea name="detail" onChange={handleChange} value={commentPost.detail} id="" cols="30" rows="10">*/}
 
-                 </textarea>
-                        <button type="submit">Summit</button>
-                    </form> : <></>
+            {/*     </textarea>*/}
+            {/*            <button type="submit">Summit</button>*/}
+            {/*        </form> : <></>*/}
 
-            }
+            {/*}*/}
+            {/*</div>*/}
+
+            <div>
+                <section style={{}}>
+                    <div className="container my-5 py-5 text-dark"
+
+                    >
+                        <div className="row d-flex justify-content-center">
+                            <div className="col-12">
+                                <div className="card">
+                                    <div className="card-body p-4">
+                                        <div className="d-flex flex-start w-100">
+
+
+                                            <div className="w-100">
+                                                <h5><img
+                                                    src="https://img.icons8.com/?size=50&id=9q3GMpxNIMjC&format=png"
+                                                    alt=""/>Thêm bình luận</h5>
+
+                                                <div className="form-outline">
+                                                    <textarea className="form-control" id="textAreaExample"
+                                                              style={{
+                                                                  backgroundColor: "#e7e3e3"
+                                                              }}
+                                                              onChange={handleChange}
+                                                              value={commentPost.detail}
+                                                              name="detail"
+                                                              rows="4"></textarea>
+
+                                                </div>
+                                                <div className="d-flex justify-content-between mt-3">
+                                                    <button type="button" className="btn"
+                                                            style={{backgroundColor: "blueviolet", color: "white"}}
+                                                            onClick={handleSummit}
+                                                    >
+                                                        Thêm
+                                                        <img width="32" height="32" src="https://img.icons8.com/external-anggara-filled-outline-anggara-putra/32/000000/external-send-social-media-interface-anggara-filled-outline-anggara-putra.png" alt="external-send-social-media-interface-anggara-filled-outline-anggara-putra"/>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
         </div>
 
